3b97e50c86c889439604e9b06c28444b5093b4dc
pete <peteryuhsin@gmail.com>
2022-09-11 20:49:29 +0800

    orders view #28

---
+++ models/historical_orders/historical_orders.sql
@@ -0,0 +1,29 @@
+with today as (
+    select 
+        distinct max(timestamp) as today_timestamp
+    from 
+        {% if target.name == 'dev' %}  dbt_traderdao.settlementprice 
+        {% elif target.name == 'prod' %}  public.settlementprice
+        {% endif %}
+),
+orders as (
+    SELECT
+        distinct orders.*,
+        today.today_timestamp
+    FROM
+        {% if target.name == 'dev' %}  dbt_traderdao.orderstable  orders
+        {% elif target.name == 'prod' %}  public.orderstable orders
+        {% endif %}
+    join today on today.today_timestamp is not null
+)
+
+select 
+    *
+from    
+    orders
+where 
+    time < today_timestamp
+
+
+
+   

fb2b17833460a3274920c231fdd12619d80677b1
pete <peteryuhsin@gmail.com>
2022-09-11 19:58:40 +0800

    updatethe naming of cum bought cost #24

--- models/order_trans/order_with_unrealized_pnl.sql
+++ models/order_trans/order_with_unrealized_pnl.sql
@@ -4,7 +4,7 @@ select
     orderscum.buy_qty_cum - orderscum.sell_qty_cum as total_qty,
     (orderscum.mark_price * (orderscum.buy_qty_cum - orderscum.sell_qty_cum)) - 
         (orderscum.buy_cost_cum - orderscum.sell_cost_cum) as unrealized,
-    cumcost.cum_sold_cost
+    cumcost.cum_bought_cost
 
 FROM {{ ref('order_cum') }} orderscum
-LEFT JOIN {{ ref('cum_sold_cost') }} cumcost on cumcost.orderid = orderscum.orderid
+LEFT JOIN {{ ref('cum_bought_cost') }} cumcost on cumcost.orderid = orderscum.orderid
